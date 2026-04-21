50cd919a79f3fe9e03991265d4ef0a61036f6532
Oleg <olegomil325@gmail.com>
2026-04-21 12:20:23 +0300

    soft assert for InputTest.java

--- src/test/java/InputTest.java
+++ src/test/java/InputTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
@@ -27,6 +28,8 @@ public class InputTest {
         WebDriver driver = new ChromeDriver(options);
         // ожидание
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //Soft Assert
+        SoftAssert softAssert = new SoftAssert();
         // открываем страницу
         driver.get("https://the-internet.herokuapp.com/inputs");
 
@@ -34,39 +37,41 @@ public class InputTest {
 
         //Проверка ввода чисел через sendkeys
         input.sendKeys("5");
-        Assert.assertEquals(input.getAttribute("value"), "5", "Должно быть введено 5");
+        softAssert.assertEquals(input.getAttribute("value"), "5", "Должно быть введено 5");
         System.out.printf("Проверка ввода чисел: %s%n", input.getAttribute("value"));
 
         //Проверка нажатия стрелки вверх 2 раза
         input.sendKeys(Keys.ARROW_UP);
         input.sendKeys(Keys.ARROW_UP);
-        Assert.assertEquals(input.getAttribute("value"), "7", "Должно быть введено 7");
+        softAssert.assertEquals(input.getAttribute("value"), "7", "Должно быть введено 7");
         System.out.printf("Проверка ввода после Keys.ARROW_UP*2: %s%n", input.getAttribute("value"));
 
         //Проверка нажатия стрелки вниз 2 раза
         input.sendKeys(Keys.ARROW_DOWN);
         input.sendKeys(Keys.ARROW_DOWN);
-        Assert.assertEquals(input.getAttribute("value"), "5", "Должно быть введено 5");
+        softAssert.assertEquals(input.getAttribute("value"), "5", "Должно быть введено 5");
         System.out.printf("Проверка ввода после Keys.ARROW_DOWN*2: %s%n", input.getAttribute("value"));
 
         //Проверка ввода букв
         input.clear();
         input.sendKeys("A");
         String result = input.getAttribute("value");
-        Assert.assertTrue(result.isEmpty() || !result.contains("A"), "Не должно быть букв в числовом поле");
+        softAssert.assertTrue(result.isEmpty() || !result.contains("A"), "Не должно быть букв в числовом поле");
         System.out.printf("Проверка ввода букв: %s%n", input.getAttribute("value"));
 
         //Проверка ввода отрицательных чисел
         input.clear();
         input.sendKeys("-5");
-        Assert.assertEquals(input.getAttribute("value"), "-5", "Должно быть введено -5");
+        softAssert.assertEquals(input.getAttribute("value"), "-5", "Должно быть введено -5");
         System.out.printf("Проверка ввода отрицательных чисел: %s%n", input.getAttribute("value"));
 
         //Проверка ввода дробных чисел
         input.clear();
         input.sendKeys("3.14");
-        Assert.assertEquals(input.getAttribute("value"), "3.14", "Должно быть введено 3.14");
+        softAssert.assertEquals(input.getAttribute("value"), "3.14", "Должно быть введено 3.14");
         System.out.printf("Проверка ввода дробных чисел: %s%n", input.getAttribute("value"));
+
+        softAssert.assertAll();
 
         driver.quit();
     }
